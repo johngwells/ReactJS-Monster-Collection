0bd43bf911eacc833d85ed12eaa7a28950515803
John Wells <molekewl2017@gmail.com>
2019-09-02 08:16:55 -0700

    broke down into smaller component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 
@@ -27,10 +29,9 @@ class App extends Component {
     );
     return (
       <div className="App">
-      <input 
-        type='search' 
+      <SearchBox 
         placeholder='search monster' 
-        onChange={e => 
+        handleChange={e => 
           this.setState({ searchField: e.target.value })}
       />
       <CardList monsters={filteredMonsters}/>
